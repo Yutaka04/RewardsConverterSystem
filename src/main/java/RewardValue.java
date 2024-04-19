47da4207405d241d5713754eea5fd3e8d83e1844
Yutaka04 <ryuyutaka04@gmail.com>
2024-04-19 12:57:45 +0800

    added MILES_TO_CASH_RATE variable under RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -4,12 +4,16 @@ public class RewardValue {
 
 	private int milesValue;
 
+	private final double MILES_TO_CASH_RATE = 0.0035;
+
 	public RewardValue(int milesValue) {
 		this.milesValue = milesValue;
+		this.cashValue = milesValue * MILES_TO_CASH_RATE;
 	}
 
 	public RewardValue(double cashValue) {
 		this.cashValue = cashValue;
+		this.milesValue = (int) (cashValue / MILES_TO_CASH_RATE);
 	}
 
 	public double getCashValue() {
